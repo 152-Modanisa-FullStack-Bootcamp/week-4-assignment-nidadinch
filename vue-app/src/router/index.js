baf0894d8ab2a50a53824040c9d146853aa025a9
Nida Dinç <20160808047@ogr.akdeniz.edu.tr>
2022-01-31 00:39:16 +0300

    delete unnecessary comments

--- vue-app/src/router/index.js
+++ vue-app/src/router/index.js
@@ -13,10 +13,7 @@ const routes = [
   {
     path: '/watch/:videoId',
     name: 'Watch',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Watch.vue')
+    component: () => import('../views/Watch.vue')
   }
 ]
 
